5bd163e958b665125438d47ea578a9981f097176
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2023-12-25 20:48:17 +0530

    Chapter 8 (Material Drawer | DevTools | ListView | NetworkImage)

--- Flutter Porjects/30_days_30_learning/flutter_application_1/lib/pages/login_page.dart
+++ Flutter Porjects/30_days_30_learning/flutter_application_1/lib/pages/login_page.dart
@@ -11,6 +11,22 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool changeBtn = false;
+  final _formKey = GlobalKey<FormState>();
+
+  //move to home methode
+  moveToHome(BuildContext context) async {
+    // how validation will be applied .
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeBtn = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changeBtn = false;
+      });
+    }
+  }
 
   //making a variable name
   @override
@@ -21,22 +37,23 @@ class _LoginPageState extends State<LoginPage> {
           title: Text("Catalog App"),
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/lg.png",
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                "Welcome",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 40.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/lg.png",
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  "Welcome $name",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 40.0),
 
-              // using of TextField and form field
-              Form(
-                child: Padding(
+                // using of TextField and form field
+                Padding(
                   padding: const EdgeInsets.symmetric(
                       vertical: 16.0, horizontal: 32.0),
                   child: Column(
@@ -45,6 +62,16 @@ class _LoginPageState extends State<LoginPage> {
                         decoration: InputDecoration(
                             hintText: "Enter User Name",
                             labelText: "User Name"),
+
+                        //* Validation for email username.
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Empty Username";
+                          } else if (value.length < 6 || value.length > 30) {
+                            return "Invalid Username";
+                          }
+                          return null;
+                        },
                         onChanged: (value) {
                           name = value;
                           setState(() {});
@@ -56,43 +83,52 @@ class _LoginPageState extends State<LoginPage> {
                           hintText: "Enter User Password",
                           labelText: "User Password",
                         ),
+
+                        //* Validation for email username.
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Empty Password";
+                          } else if (value.length < 6 || value.length > 15) {
+                            return "Invalid Password";
+                          }
+                          return null;
+                        },
                       ),
                       SizedBox(height: 60.0),
 
-                      InkWell(
-                        // onTap: () => Navigator.pushNamed(context, "/homepage"),
-                        onTap: () async {
-                          setState(() {
-                            changeBtn = true;
-                          });
-                          await Future.delayed(Duration(seconds: 1));
-                          Navigator.pushNamed(context, MyRoutes.homeRoute);
-                        },
-                        child: AnimatedContainer(
-                          duration: Duration(seconds: 1),
-                          width: changeBtn ? 50 : 150,
-                          height: 50,
-                          alignment: Alignment.center,
-                          child: changeBtn
-                              ? Icon(
-                                  Icons.done,
-                                  color: Colors.white,
-                                )
-                              : Text(
-                                  "LOGIN",
-                                  style: TextStyle(
+                      Material(
+                        color: Colors.deepPurple,
+                        borderRadius:
+                            BorderRadius.circular(changeBtn ? 50.0 : 8.0),
+                        child: InkWell(
+                          // onTap: () => Navigator.pushNamed(context, "/homepage"),
+                          onTap: () => moveToHome(context),
+                          child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            width: changeBtn ? 50 : 150,
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: changeBtn
+                                ? Icon(
+                                    Icons.done,
                                     color: Colors.white,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
+                                  )
+                                : Text(
+                                    "LOGIN",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                          decoration: BoxDecoration(
-                              color: Colors.purple,
-                              // shape: changeBtn
-                              //     ? BoxShape.circle
-                              //     : BoxShape.rectangle,
-                              borderRadius: BorderRadius.circular(
-                                  changeBtn ? 50.0 : 8.0)),
+                            // decoration: BoxDecoration(
+                            //     color: Colors.purple,
+                            //     // shape: changeBtn
+                            //     //     ? BoxShape.circle
+                            //     //     : BoxShape.rectangle,
+                            //     borderRadius: BorderRadius.circular(
+                            //         changeBtn ? 50.0 : 8.0)),
+                          ),
                         ),
                       ),
 
@@ -110,8 +146,8 @@ class _LoginPageState extends State<LoginPage> {
                     ],
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
