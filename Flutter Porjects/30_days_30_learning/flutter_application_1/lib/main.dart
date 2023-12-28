c2c787d1214dc12562d89a6109da282188541843
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2023-12-28 14:06:12 +0530

    Day 12 - ListView Builder, List Generate, Card & Asserts

--- Flutter Porjects/30_days_30_learning/flutter_application_1/lib/main.dart
+++ Flutter Porjects/30_days_30_learning/flutter_application_1/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
 import 'package:flutter_application_1/utils/routes.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_application_1/widgets/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily,
-          primaryTextTheme: GoogleFonts.latoTextTheme()),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
 
       // home: HomePage(),
       debugShowCheckedModeBanner: false,
--- Flutter Porjects/30_days_30_learning/flutter_application_1/lib/pages/home_page.dart
+++ Flutter Porjects/30_days_30_learning/flutter_application_1/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/model/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+import 'package:flutter_application_1/widgets/item_widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,14 +11,21 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dumyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Cataloge App"),
       ),
-      body: Center(
-          child: Container(
-        child: Text("This is $days $course learning Challange"),
-      )),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dumyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dumyList[index],
+              );
+            }),
+      ),
       drawer: MyDrawer(
           // backgroundColor: Colors.amber,
           // elevation: 3.23,
