26ad2aa2715e14bdde7e2a30073d468433739894
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2024-01-11 01:00:50 +0530

    REST API AP

---
+++ Flutter Porjects/rest_tutorial/lib/homePage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('REST API '),
+        centerTitle: true,
+        elevation: 10.0,
+      ),
+    );
+  }
+}
